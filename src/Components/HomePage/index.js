a86a54ae7490d14341e8071c4326eb1703d352ff
pankajladhar <ladharpankaj@gmail.com>
2019-03-17 22:29:19 +0530

    Update giving back to society section

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -29,9 +29,17 @@ const HomePage = props => {
                             <strong>Giving </strong>
                             back to society
                         </h1>
-                        <p>
-                            Feel proud when you buy a mango from us because you are helping in contributing to an Institution for the Aged  – TARA in Rajarajeshwari Nagar which is run by the bangalore freemason’s society.
-                        </p>
+                        <div className="content">
+                            <div className="left__content">
+                                <p>
+                                Feel proud when you buy a mango 
+                                from us because you are helping in contributing to an 
+                                Institution for the Aged  – TARA in Rajarajeshwari Nagar 
+                                which is run by the bangalore freemason’s society.
+                                </p>
+                            </div>
+                            <div className="left__content"></div>
+                        </div>
                     </div>
                 </div>
                 <Certificates />
