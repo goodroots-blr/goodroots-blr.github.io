4dc43168b2842b0dfc32b217ad58168de7e15a73
pankajladhar <ladharpankaj@gmail.com>
2019-03-18 22:31:46 +0530

    Refactor Giving back section

--- src/Route/HomePage/HomePage.js
+++ src/Route/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import {
     heroSliderData,
     companyRelatedData,
     certificatesData,
+    givingBackData,
     footerData
 } from './../../resources/data';
 import HeroSlider from './../../Components/_UI/HeroSlider/HeroSlider';
@@ -12,12 +13,36 @@ import SrollToTop from './../../Components/_UI/SrollToTop/SrollToTop';
 // import WhoWeAre from './../../Components/_Section/WhoWeAre';
 // import WhyMango from './../../Components/_Section/WhyMango';
 // import OurFarm from './../../Components/_Section/OurFarm';
-// import givinbackImg from './../../resources/images/givinback.jpg';
 // // import OurProducts from './../OurProducts';
 import Certificates from './../../Components/_Section/Certificates/Certificates';
 import Footer from './../../Components/_Section/Footer/Footer';
 import CopyrightFooter from './../../Components/_Section/CopyrightFooter/CopyrightFooter';
 import './HomePage.scss'
+
+const renderGivingBack = () => (
+    <div className="givingBack">
+        <div className="container">
+            <h1 className="main-title">
+                <strong>Giving </strong>
+                back to society
+                        </h1>
+            <div className="content">
+                <div className="left__content">
+                    {
+                        givingBackData.paragraphs.map((para, index) => {
+                            return (
+                                <p key={index}>{para}</p>
+                            )
+                        })
+                    }
+                </div>
+                <div className="left__content">
+                    <img src={givingBackData.img} alt={givingBackData.alt} />
+                </div>
+            </div>
+        </div>
+    </div>
+)
 
 const HomePage = () => {
     return (
@@ -30,30 +55,11 @@ const HomePage = () => {
                 <OurFarm />
                 <WhyMango /> */}
             {/* <OurProducts /> */}
-            {/* <div className="givingBack">
-                    <div className="container">
-                        <h1 className="main-title">
-                            <strong>Giving </strong>
-                            back to society
-                        </h1>
-                        <div className="content">
-                            <div className="left__content">
-                                <p>
-                                    Feel proud when you buy a mango
-                                    from us because you are helping in contributing to an
-                                    Institution for the Aged  – TARA in Rajarajeshwari Nagar
-                                    which is run by the bangalore freemason’s society.
-                                </p>
-                            </div>
-                            <div className="left__content">
-                                <img src={givinbackImg} alt="Giving back to society" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            {/* 
             */}
             <main>
-            <Certificates data={certificatesData} />
+                {renderGivingBack()}
+                <Certificates data={certificatesData} />
             </main>
             <Footer data={footerData} />
             <CopyrightFooter content={footerData.copyrightText()} />
--- src/resources/data/index.js
+++ src/resources/data/index.js
@@ -2,10 +2,12 @@ import heroSliderData from './HeroSlider';
 import companyRelatedData from './CompanyRelated';
 import footerData from './Footer';
 import certificatesData from './Certificates';
+import givingBackData from './GivingBack';
 
 export {
     heroSliderData,
     companyRelatedData,
     certificatesData,
-    footerData
+    footerData,
+    givingBackData
 }
