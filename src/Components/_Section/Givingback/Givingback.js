73587800b4cfde541f9c52c39c20ef9a37e2408d
pankajladhar <ladharpankaj@gmail.com>
2019-03-19 22:19:57 +0530

    Add giving back section

---
+++ src/Components/_Section/Givingback/Givingback.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Givingback.scss'
+
+const Givingback = ({ data }) => {
+    return (
+        <div className="givingBack">
+            <div className="container">
+                <h1 className="main-title">
+                    {data.title()}
+                </h1>
+                <div className="content">
+                    <div className="left__content">
+                        {
+                            data.paragraphs.map((para, index) => {
+                                return (
+                                    <p key={index}>{para}</p>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="left__content">
+                        <img src={data.img} alt={data.alt} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Givingback;
