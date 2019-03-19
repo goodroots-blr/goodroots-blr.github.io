fec34ca1386cb720dc3af754f96849e72da952b2
pankajladhar <ladharpankaj@gmail.com>
2019-03-19 20:39:06 +0530

    Add pre book button in mobile

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeroSlider from './../HeroSlider';
 import Header from './../Header';
 import AboutUs from './../AboutUs';
 import WhoWeAre from './../WhoWeAre';
 import WhyMango from './../WhyMango';
 import OurFarm from './../OurFarm';
+import Button from './../Button'
+import ProductOverlay from './../ProductOverlay'
 import givinbackImg from './../../resources/images/givinback.jpg';
 // import OurProducts from './../OurProducts';
 import Certificates from './../Certificates';
@@ -14,6 +16,15 @@ import CopyrightFooter from './../CopyrightFooter';
 import './HomePage.scss'
 
 const HomePage = props => {
+    const [toggle, setToggle] = useState(false);
+    const handleClick = () => {
+        document.body.classList.add('productOverlay--open');
+        setToggle(true);
+    }
+    const hideMenu = () => {
+        setToggle(false);
+        document.body.classList.remove('productOverlay--open');
+    }
     return (
         <div className="app">
             <Header />
@@ -35,7 +46,7 @@ const HomePage = props => {
                                 <p>
                                     Feel proud when you buy a mango
                                     from us because you are helping in contributing to an
-                                    Institution for the Aged  – TARA in Rajarajeshwari Nagar
+                                    Institution for the Aged  – <strong>TARA</strong> in Rajarajeshwari Nagar
                                     which is run by the bangalore freemason’s society.
                                 </p>
                             </div>
@@ -43,6 +54,18 @@ const HomePage = props => {
                                 <img src={givinbackImg} alt="Giving back to society" />
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="mobile-prebook mobileOnly">
+                    <div className="container">
+                        <p>
+                            You can prebook your mangoes today
+                        </p>
+                        <Button title="Prebook now"
+                            type="hero"
+                            inverse
+                            onClick={handleClick} />
+                        {toggle && <ProductOverlay hideMenu={hideMenu} />}
                     </div>
                 </div>
                 <Certificates />
