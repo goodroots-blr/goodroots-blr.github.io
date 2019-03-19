97b63fd160ea8ca4ac174cc17d09f8fde8aba7c9
pankajladhar <ladharpankaj@gmail.com>
2019-03-19 20:43:12 +0530

    Fix content

--- src/resources/data/WhoWeAre.js
+++ src/resources/data/WhoWeAre.js
@@ -8,13 +8,9 @@ export default {
     subTitle: "welcome to GoodRoots - we serve Alphonso mango fruits",
     paragraphs: [
         `We are Second generation mango growers with farm in a small 
-    town near bellary. Our mission is to bring back the 
-    real taste of naturally ripened Ratnagiri Alphonso mango to true mango lovers.`,
+        town near bellary. Our mission is to bring back the 
+        real taste of naturally ripened Ratnagiri Alphonso mango to true mango lovers.`,
         `We are happy to bring our premium category of Ratnagiri Alphonso mangoes 
-    which have been special taken care in our farm and delivered to you.`,
-        `Who Doesn’t value good service at a reasonable price and we are no different. 
-     We ensure that you get top quality product and service at the most 
-     reasonable price. We bring these mangoes directly from our farms 
-     in semi ripened condition and have it delivered to your home.`
+        which have been special taken care in our farm and delivered to you.`
     ]
 }
