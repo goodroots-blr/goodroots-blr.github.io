ee3e8a02b6e1b8b4db545ed6516f7faaf843934e
pankajladhar <ladharpankaj@gmail.com>
2019-03-24 14:03:27 +0530

    Update content

--- src/resources/data/Home.js
+++ src/resources/data/Home.js
@@ -1,5 +1,5 @@
 export default {
-    paragraph: `Red soils of Karnataka and firece hot temperature of 
+    paragraph: `Red soils of Karnataka and fierce hot temperature of 
     Bellary coupled with the help of decades of best organic agricultural 
     practice - we bring the best of Ratnagiri Alphonso 
     Mangoes and Banganapalli Mangoes to your door step in Namma Bengaluru. Yes you read it right!`,
--- src/resources/data/WhoWeAre.js
+++ src/resources/data/WhoWeAre.js
@@ -6,15 +6,12 @@ export default {
     alt: "who we are",
     title: () => <><strong>Who</strong> we are</>,
     subTitle: "welcome to GoodRoots - we produce organic Alphonso & Banganapalli mangoes",
-    paragraph: `We are Third generation mango growers with farm in a small 
-    town near bellary. Our mission is to bring back the 
-    real taste of naturally ripened Alphonso & Banganapalli mangoes to true mango lovers.`,
-    paragraph1: `We are happy to bring our premium category of Ratnagiri Alphonso & Banganapalli mangoes 
-    which have been taken care specially in our farm and delivered to you.`,
-     paragraph2: `Who Doesn’t value good service at a reasonable price and we are no different. 
-     We ensure that you get top quality product and service at the most 
-     reasonable price. We bring these mangoes directly from our farms 
-     in semi ripened condition and have it delivered to your home.`
+    paragraph: `We are the third generation cultivating our ancestral mango 
+    farm in a small town near Bellary. Our mission is to 
+    bring back the real taste of naturally ripened Ratnagiri Alphonso and Banaganapalli mangoes 
+    to true mango lovers.`,
+    paragraph1: `We are happy to bring our premium category of Ratnagiri Alphonso and Banaganapalli mangoes, 
+    which have been taken utmost care of in our farm and delivered to your doorstep.`    
 }
 
 // listItems: [
